526ab90390501532b9beb3eb405ad418ea63dcd9
Victor Bahlangene <victorBahlangene96@gmail.com>
2018-05-01 22:54:27 +0300

    found corrent api url path

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
 
 document.getElementById("btn").addEventListener("click",() => {
 
-
-    let url = "https://wind-bow.gomix.me/twitch-api" ;
+    //example for savijs twitch channel id
+    let urlTest = "https://wind-bow.glitch.me/twitch-api/streams/43131877";
+    
+    // to get twitch channel id use https://wind-bow.glitch.me/twitch-api/channels/<channel name>
+    
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", url, true);
+    xhttp.open("GET", urlTest, true);
     xhttp.onload = function () {
         if (this.status == 200) {
             let result = JSON.parse(this.responseText);
-            console.log(result);
+            //console.log(result);
+            // displays message if stream is ofline
+            if (result.stream == null) {
+                console.log("test worked");
+            } else {
+                console.log("user is online");
+            }
         }
     };
     xhttp.send();
 
 });
+
+// to do list //
+//1) add 4 more streams to be test examples
+//2) add links to actual twitch page
+//3) add twitch logo for each channel etc
 
